e939fd87c727ad460412daf3391bdda742b2dab8
YanTylets <tylets@gmail.com>
2022-01-15 02:47:36 +0300

    finish weather app react native

--- App.js
+++ App.js
@@ -1,20 +1,49 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Alert } from 'react-native';
+import * as Location from 'expo-location';
+import axios from 'axios';
+import Loading from './Loading';
+import { View } from 'react-native-web';
+import Weather from './Weather';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+const API_KEY = 'db31f62cbcccec97aac3bf68068df580';
+
+export default class extends React.Component {
+
+  state = {
+    isLoading: true
+  }
+
+  getWeather = async (latitude, longitude) => {
+    const {data: {main: {temp}, weather}} = await axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`);
+    this.setState({isLoading: false, temp:temp, condition: weather[0].main});
+    console.log(temp, weather[0].main)
+  }
+
+  getLocation = async () => {
+    try {
+      await Location.requestForegroundPermissionsAsync();
+      const {coords: {latitude, longitude}} = await Location.getCurrentPositionAsync();
+      this.getWeather(latitude, longitude);
+    } catch (error) {
+      Alert.alert('Не могу определить местоположение', "Оче нь грустно :(")
+    }
+
+  };
+
+  componentDidMount() {
+    this.getLocation();
+  }
+  render () { 
+    return (
+      this.state.isLoading ?
+      <Loading/> : 
+      <Weather 
+      temp={Math.round(this.state.temp)}
+      condition={this.state.condition}
+
+      />
+    );
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
